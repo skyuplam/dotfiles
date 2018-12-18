a9a9b7e568a361983f98c27cc3b0e72dc94abaf2
Terrence Lam <skyuplam@gmail.com>
2018-12-18 15:31:41 +0100

    Update spacemacss org mode

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -50,7 +50,9 @@ values."
      jsx
      git
      markdown
-     org
+     (org :variables
+          org-enable-github-support t
+          org-projectile-file "TODOs.org")
      ;; (shell :variables
      ;;        shell-default-height 30
      ;;        shell-default-position 'bottom)
