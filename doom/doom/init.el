a028f7e52660396df45990c51a766572e3109dcc
Terrence Lam <skyuplam@gmail.com>
2019-02-21 13:27:34 +0100

    Remove unused doom modules

--- doom/doom/init.el
+++ doom/doom/init.el
@@ -141,13 +141,13 @@
        ;; toward a specific purpose. They may have additional dependencies and
        ;; should be loaded late.
        :app
-       (email +gmail)      ; emacs as an email client
+       ;;(email +gmail)      ; emacs as an email client
        ;;irc               ; how neckbeards socialize
        (rss +org)        ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
-       (write            ; emacs as a word processor (latex + org + markdown)
-        +wordnut         ; wordnet (wn) search
-        +langtool)       ; a proofreader (grammar/style check) for Emacs
+       ;;(write            ; emacs as a word processor (latex + org + markdown)
+       ;; +wordnut         ; wordnet (wn) search
+       ;; +langtool)       ; a proofreader (grammar/style check) for Emacs
        notmuch
 
        :collab
