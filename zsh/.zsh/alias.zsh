5754d397c153a157ce8d1d300412d0e4e8a5ee78
Terrence Lam <skyuplam@gmail.com>
2019-03-24 17:08:46 +0100

    Add osx specific alias

--- zsh/.zsh/alias.zsh
+++ zsh/.zsh/alias.zsh
@@ -19,3 +19,20 @@ fi
 if type emacs > /dev/null; then
   alias emacs='emacs -nw'
 fi
+
+
+if [ "$(uname 2> /dev/null)" = "Darwin" ]; then
+  function man-preview() {
+    man -t "$@" | open -f -a Preview
+  }
+
+  function quick-look() {
+    (( $# > 0 )) && qlmanage -p $* &>/dev/null &
+  }
+
+  # Man page to Preview
+  alias manp='man-preview'
+  # Show/hide hidden files in the Finder
+  alias showfiles="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+  alias hidefiles="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
+fi
