c8b34008990ec7f767e15c1b5ee5be6ff0cf0cf4
Terrence Lam <skyuplam@gmail.com>
2019-05-30 09:05:26 +0200

    Update doom config

--- doom/doom/init.el
+++ doom/doom/init.el
@@ -58,8 +58,6 @@
 
        electric          ; smarter, keyword-based electric-indent
        ;;eshell            ; a consistent, cross-platform shell (WIP)
-       imenu             ; an imenu sidebar and searchable code index
-       term              ; terminals in Emacs
        vc                ; version-control and Emacs, sitting in a tree
 
        :tools
@@ -73,20 +71,18 @@
        (flycheck +childframe)
        flyspell
        ;;gist              ; interacting with github gists
-       (lookup           ; helps you navigate your code and documentation
-        +docsets)        ; ...or in Dash docsets locally
+       (lookup             ; helps you navigate your code and documentation
+        +docsets)          ; ...or in Dash docsets locally
        ;;macos             ; MacOS-specific commands
        make                ; run make tasks from Emacs
        magit               ; a git porcelain for Emacs
-       (password-store
-         +auth)            ; password manager for nerds
+       pass                ; password manager for nerds
        ;;pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        rgb               ; creating color strings
        ;;terraform         ; infrastructure as code
        tmux                ; an API for interacting with tmux
        ;;upload            ; map local to remote projects via ssh/ftp
-       vterm
        ;;wakatime
 
        :lang
@@ -141,6 +137,10 @@
        ;;swift             ; who asked for emoji variables?
        web                 ; the tubes
        ;;vala              ; GObjective-C
+
+       :term
+       term                ; terminals in Emacs
+       vterm
 
        :email
        ;;notmuch
