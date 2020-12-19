527e3aaa598e8d1fb33b2b78510e7d5c75f6aa88
Terrence Lam <skyuplam@gmail.com>
2020-12-19 21:34:51 +0100

    feat(zsh): add fzf git key bindings

--- zsh/.zsh/alias.zsh
+++ zsh/.zsh/alias.zsh
@@ -54,13 +54,6 @@ if [ "$(uname 2> /dev/null)" = "Darwin" ]; then
   alias hidefiles="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
 fi
 
-# skim with preview
-# function skk() {
-#   if [ -n "$1" ]
-#   then
-#     sk --ansi -i -c "rg --line-number --column --color \"always\" $1" --preview "$FZF_PREVIEW_SH {}" fi
-# }
-
 # new Zettelkasten note
 function zet() {
   if [ -n "$1" ]; then
@@ -105,3 +98,82 @@ function v() {
   local file
   file="$(fasd -Rfl "$1" | fzf -1 -0 --no-sort -m)" && ${EDITOR:-vim} "${file}" || return 1
 }
+
+# GIT heart FZF
+# https://gist.github.com/junegunn/8b572b8d4b5eddd8b85e5f4d40f17236
+# -------------
+
+is_in_git_repo() {
+  git rev-parse HEAD > /dev/null 2>&1
+}
+
+fzf-down() {
+  fzf --height 50% "$@" --border
+}
+
+_gf() {
+  is_in_git_repo || return
+  git -c color.status=always status --short |
+  fzf-down -m --ansi --nth 2..,.. \
+    --preview '(git diff --color=always -- {-1} | sed 1,4d; cat {-1}) | head -500' |
+  cut -c4- | sed 's/.* -> //'
+}
+
+_gb() {
+  is_in_git_repo || return
+  git branch -a --color=always | grep -v '/HEAD\s' | sort |
+  fzf-down --ansi --multi --tac --preview-window right:70% \
+    --preview 'git log --oneline --graph --date=short --color=always --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1) | head -'$LINES |
+  sed 's/^..//' | cut -d' ' -f1 |
+  sed 's#^remotes/##'
+}
+
+_gt() {
+  is_in_git_repo || return
+  git tag --sort -version:refname |
+  fzf-down --multi --preview-window right:70% \
+    --preview 'git show --color=always {} | head -'$LINES
+}
+
+_gh() {
+  is_in_git_repo || return
+  git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
+  fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
+    --header 'Press CTRL-S to toggle sort' \
+    --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | head -'$LINES |
+  grep -o "[a-f0-9]\{7,\}"
+}
+
+_gr() {
+  is_in_git_repo || return
+  git remote -v | awk '{print $1 "\t" $2}' | uniq |
+  fzf-down --tac \
+    --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" {1} | head -200' |
+  cut -d$'\t' -f1
+}
+
+# Key-binding
+#
+# CTRL-GCTRL-F for files
+# CTRL-GCTRL-B for branches
+# CTRL-GCTRL-T for tags
+# CTRL-GCTRL-R for remotes
+# CTRL-GCTRL-H for commit hashes
+
+join-lines() {
+  local item
+  while read item; do
+    echo -n "${(q)item} "
+  done
+}
+
+bind-git-helper() {
+  local c
+  for c in $@; do
+    eval "fzf-g$c-widget() { local result=\$(_g$c | join-lines); zle reset-prompt; LBUFFER+=\$result }"
+    eval "zle -N fzf-g$c-widget"
+    eval "bindkey '^g^$c' fzf-g$c-widget"
+  done
+}
+bind-git-helper f b t r h
+unset -f bind-git-helper
