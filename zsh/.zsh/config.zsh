4d360604f0e8f2d7c1685e4e0ffacfb13ffc3290
Terrence Lam <skyuplam@gmail.com>
2020-12-27 23:23:01 +0100

    fix(zsh): remove unused env var

--- zsh/.zsh/config.zsh
+++ zsh/.zsh/config.zsh
@@ -11,11 +11,6 @@ export EDITOR=nvim
 export VISUAL=nvim
 export BROWSER=firefox-nightly
 
-# IBus
-export GTK_IM_MODULE=ibus
-export XMODIFIERS='@im ibus'
-export QT_IM_MODULE=ibus
-
 # GPG
 export GPG_TTY=$(tty)
 
