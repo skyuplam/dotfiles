792984915cb727d5b47f6c09ddb6752663bfd103
Terrence Lam <terrence@tlam.dev>
2022-06-02 09:34:05 +0200

    feat(nvim): ignore binary file preview

--- nvim/nvim/lua/_/telescope.lua
+++ nvim/nvim/lua/_/telescope.lua
@@ -4,10 +4,37 @@ if not has_telescope then return end
 
 local M = {}
 
+local previewers = require('telescope.previewers')
+local Job = require('plenary.job')
+
+-- Ignore binary files
+local new_maker = function(filepath, bufnr, opts)
+  opts = opts or {}
+
+  filepath = vim.fn.expand(filepath)
+
+  Job:new({
+    command='file',
+    args={'--mime-type', '-b', filepath},
+    on_exit=function(j)
+      local mime_type = vim.split(j:result()[1], '/')[1]
+
+      if mime_type == 'text' then
+        previewers.buffer_previewer_maker(filepath, bufnr, opts)
+      else
+        -- maybe we want to write something to the buffer here
+        vim.schedule(function()
+          vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, {'BINARY'})
+        end)
+      end
+    end
+  }):sync()
+end
+
 local function setup()
 
   telescope.setup({
-    defaults={layout_strategy='flex'},
+    defaults={layout_strategy='flex', buffer_previewer_maker=new_maker},
     extensions={
       fzf={
         fuzzy=true,
