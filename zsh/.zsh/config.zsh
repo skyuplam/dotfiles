4d2d88cff82f318b6253ebe91aa0fdef49775df5
Terrence Lam <terrence@tlam.dev>
2022-12-19 14:41:31 +0100

    chore(zsh): add more XDG home env var

--- zsh/.zsh/config.zsh
+++ zsh/.zsh/config.zsh
@@ -16,7 +16,9 @@ export GPG_TTY=$(tty)
 
 # XDG
 export XDG_CONFIG_HOME=$HOME/.config
+export XDG_CACHE_HOME=$HOME/.cache
 export XDG_DATA_HOME=$HOME/.local/share
+export XDG_STATE_HOME=$HOME/.local/state
 
 typeset -U path
 path=(/usr/local/sbin /usr/local/bin $path[@])
