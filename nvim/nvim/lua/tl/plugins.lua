cba39abc06b531d8faf745630e51a3d2107c9d09
Terrence Lam <terrence@tlam.dev>
2022-12-21 22:29:30 +0100

    feat(nvim): add which-key

--- nvim/nvim/lua/tl/plugins.lua
+++ nvim/nvim/lua/tl/plugins.lua
@@ -31,6 +31,16 @@ return require('packer').startup(function(use)
     }
   }
   use {'folke/tokyonight.nvim'}
+  use {
+    'folke/which-key.nvim',
+    config=function()
+      require('which-key').setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
   use {'vim-jp/syntax-vim-ex'}
 
   use {
