a1d71b61b987232c21e2afa4fb386bb067058ea8
Terrence Lam <terrence@tlam.dev>
2023-03-11 09:22:17 +0100

    refactor(nvim): cleanup nvim tree leftover

--- nvim/nvim/lua/tl/statusline.lua
+++ nvim/nvim/lua/tl/statusline.lua
@@ -137,10 +137,7 @@ local WorkDir = {
     end
   end,
   hl={fg='blue', bold=true},
-  on_click={
-    callback=function() vim.cmd('NvimTreeToggle') end,
-    name='heirline_workdir'
-  },
+  on_click={callback=function() vim.cmd('Xplr') end, name='heirline_workdir'},
 
   flexible=1,
   {
@@ -926,36 +923,7 @@ local TabPages = {
   TabpageClose
 }
 
-local TabLineOffset = {
-  condition=function(self)
-    local win = vim.api.nvim_tabpage_list_wins(0)[1]
-    local bufnr = vim.api.nvim_win_get_buf(win)
-    self.winid = win
-
-    if vim.api.nvim_buf_get_option(bufnr, 'filetype') == 'NvimTree' then
-      self.title = 'NvimTree'
-      return true
-    end
-  end,
-
-  provider=function(self)
-    local title = self.title
-    local width = vim.api.nvim_win_get_width(self.winid)
-    local pad = math.ceil((width - #title) / 2)
-    return string.rep(' ', pad) .. title .. string.rep(' ', pad)
-  end,
-
-  hl=function(self)
-    if vim.api.nvim_get_current_win() == self.winid then
-      return 'TablineSel'
-    else
-      return 'Tabline'
-    end
-  end
-}
-
 local TabLine = {
-  TabLineOffset,
   BufferLine,
   -- utils.make_flexible_component(1, {provider = 'aaaaaaaaaaaaaaaaaaa'}, {provider = 'bbbb'}),
   TabPages
