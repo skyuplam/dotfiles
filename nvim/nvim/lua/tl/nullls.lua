c95cabc0281594d9381bd2f3ef5539087dafea8d
Terrence Lam <terrence@tlam.dev>
2023-05-22 13:45:53 +0200

    fix(nvim): null-ls yarn pnp prettier

--- nvim/nvim/lua/tl/nullls.lua
+++ nvim/nvim/lua/tl/nullls.lua
@@ -1,10 +1,12 @@
 local has_null_ls, null_ls = pcall(require, 'null-ls')
 
 if not has_null_ls then return end
+local command_resolver = require("null-ls.helpers.command_resolver")
 
 local sources = {
-    null_ls.builtins.formatting.prettier,
-    null_ls.builtins.formatting.lua_format,
+    null_ls.builtins.formatting.prettier.with({
+        dynamic_command = command_resolver.from_yarn_pnp()
+    }), null_ls.builtins.formatting.lua_format,
     null_ls.builtins.diagnostics.write_good,
     null_ls.builtins.code_actions.gitsigns
 }
