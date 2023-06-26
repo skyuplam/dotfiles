6a3bdbb3b0958f4e04b028ea521f27ecbb8d3be4
Terrence Lam <terrence@tlam.dev>
2023-06-26 16:26:48 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -8,8 +8,6 @@
     darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    # neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
 
   outputs = { self, darwin, nixpkgs, home-manager }:
@@ -19,12 +17,29 @@
         allowUnfree = true;
         experimental-features = "nix-command flakes";
       };
-      # overlays = [ neovim-nightly-overlay.overlay ];
     };
   in
   {
     darwinConfigurations."tlammbp" = darwin.lib.darwinSystem {
       system = "x86_64-darwin";
+      modules = [
+        ./darwin.nix
+        home-manager.darwinModules.home-manager
+        (
+          { lib, pkgs, ... }: {
+            nixpkgs = nixpkgsConfig;
+            # `home-manager` config
+            users.users.terrencelam.home = "/Users/terrencelam";
+            home-manager.useGlobalPkgs = true;
+            home-manager.users.terrencelam = {
+              imports = [ ./home.nix ];
+            };
+          }
+        )
+      ];
+    };
+    darwinConfigurations."tlamm2" = darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
       modules = [
         ./darwin.nix
         home-manager.darwinModules.home-manager
