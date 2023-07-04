9ec1d3d0295c6858fdbada427b7213ba0c9e2ffc
Terrence Lam <terrence@tlam.dev>
2023-07-04 11:01:12 +0200

    refacto(nvim): use common map

--- nvim/nvim/lua/tl/term.lua
+++ nvim/nvim/lua/tl/term.lua
@@ -1,26 +1,28 @@
 local has_toggle_term, toggle_term = pcall(require, 'toggleterm')
+local map = require('tl.common').map
 
 local M = {}
 
 local Terminal = require('toggleterm.terminal').Terminal
 
 local tig = Terminal:new({
-  cmd='tig status',
-  hidden=true,
-  direction='float',
-  float_opts={border=tl.style.current.border}
+    cmd = 'tig status',
+    hidden = true,
+    direction = 'float',
+    float_opts = {border = tl.style.current.border}
 })
 
 function M.toggleTig()
-  if not has_toggle_term then return end
-  tig:toggle()
+    if not has_toggle_term then return end
+    tig:toggle()
 end
 
 function M.setup()
-  if not has_toggle_term then return end
+    if not has_toggle_term then return end
 
-  toggle_term.setup({})
-  vim.keymap.set('n', '<leader>gs', M.toggleTig, {silent=true, noremap=true})
+    toggle_term.setup({})
+    map('n', '<leader>gs', M.toggleTig,
+        {silent = true, noremap = true, desc = 'Open Tig'})
 end
 
 return M
