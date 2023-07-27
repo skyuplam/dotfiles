ba3394f80af3328aab1a1b1de17f051d8113a1f0
Terrence Lam <terrence@tlam.dev>
2023-07-27 10:37:54 +0200

    feat(nvim): add smoothcursor

--- nvim/nvim/lua/tl/plugins.lua
+++ nvim/nvim/lua/tl/plugins.lua
@@ -154,6 +154,65 @@ return require('lazy').setup({
   { 'inkarkat/vim-SyntaxRange' },
 
   {
+    'gen740/SmoothCursor.nvim',
+    config = function()
+      require('smoothcursor').setup({
+        disable_float_win = true,
+        priority = 100,
+        autostart = true,
+        threshold = 1,
+        speed = 30,
+        type = 'default',
+        intervals = 50,
+        fancy = {
+          enable = true,
+          head = { cursor = '', texthl = 'SmoothCursor' },
+          body = {
+            { cursor = '●', texthl = 'SmoothCursorBody' },
+            { cursor = '●', texthl = 'SmoothCursorBody' },
+            { cursor = '•', texthl = 'SmoothCursorBody' },
+            { cursor = '•', texthl = 'SmoothCursorBody' },
+            { cursor = '∙', texthl = 'SmoothCursorBody' },
+            { cursor = '∙', texthl = 'SmoothCursorBody' },
+          },
+        },
+      })
+
+      local autocmd = vim.api.nvim_create_autocmd
+      local augroup = vim.api.nvim_create_augroup('CursorModeChangeGroup', {})
+
+      vim.api.nvim_clear_autocmds({ group = augroup })
+      autocmd({ 'ModeChanged' }, {
+        group = augroup,
+        callback = function()
+          local current_mode = vim.fn.mode()
+          if current_mode == 'n' then
+            vim.api.nvim_set_hl(0, 'SmoothCursor', { fg = '#8aa872' })
+            vim.api.nvim_set_hl(0, 'SmoothCursorBody', { fg = '#8aa872' })
+            vim.fn.sign_define('smoothcursor', { text = '' })
+          elseif current_mode == 'v' then
+            vim.api.nvim_set_hl(0, 'SmoothCursor', { fg = '#bf616a' })
+            vim.api.nvim_set_hl(0, 'SmoothCursorBody', { fg = '#bf616a' })
+            vim.fn.sign_define('smoothcursor', { text = '' })
+          elseif current_mode == 'V' then
+            vim.api.nvim_set_hl(0, 'SmoothCursor', { fg = '#bf616a' })
+            vim.api.nvim_set_hl(0, 'SmoothCursorBody', { fg = '#bf616a' })
+            vim.fn.sign_define('smoothcursor', { text = '' })
+          elseif current_mode == '' then
+            vim.api.nvim_set_hl(0, 'SmoothCursor', { fg = '#bf616a' })
+            vim.api.nvim_set_hl(0, 'SmoothCursorBody', { fg = '#bf616a' })
+            vim.fn.sign_define('smoothcursor', { text = '' })
+          elseif current_mode == 'i' then
+            vim.api.nvim_set_hl(0, 'SmoothCursor', { fg = '#668aab' })
+            vim.api.nvim_set_hl(0, 'SmoothCursorBoby', { fg = '#668aab' })
+            vim.fn.sign_define('smoothcursor', { text = '' })
+          end
+        end,
+      })
+    end,
+  },
+
+  {
     'echasnovski/mini.nvim',
     config = function()
       require('mini.ai').setup({})
