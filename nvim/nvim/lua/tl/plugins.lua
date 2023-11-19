16133fa215568b524a6af6369165037b7dcb7c2f
Terrence Lam <terrence@tlam.dev>
2023-11-19 08:02:32 +0100

    feat(nvim): add markdown preview using peak

--- nvim/nvim/lua/tl/plugins.lua
+++ nvim/nvim/lua/tl/plugins.lua
@@ -523,5 +523,16 @@ return require('lazy').setup({
       require('femaco').setup()
     end,
   },
+  {
+    'toppair/peek.nvim',
+    event = { 'VeryLazy' },
+    build = 'deno task --quiet build:fast',
+    config = function()
+      require('peek').setup()
+      -- refer to `configuration to change defaults`
+      vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
+      vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
+    end,
+  },
 })
 ---}}}
